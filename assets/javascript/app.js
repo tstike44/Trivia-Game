2752bbba9421a1f6565b5b124ffd17a317e4955d
tstike44 <you@example.com>
2019-03-20 21:26:28 -0700

    added some bootstrap to the html...forms aren't working on multiple lines

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//need questions
+//need answers
+//reset function (start over)
+//start function
+//timer function
+//change screens interval function
+//need a time element (maybe function)
+//start screen with start button (click to start the game)
+//next screen displays the time remaining, question, possible answers
+//result screen shows the time that was remaining when the answer was clicked, whether or not they got the question right or not, the correct asnwer, and a gif
+//questions don't need to randomize 
+var questions = [
+    { q: "The sky is blue.", a: "bologna" },
+    { q: "There are 365 days in a year.", a: "cheetos" },
+    { q: "There are 42 ounces in a pound.", a: "macaroni" },
+    { q: "The Declaration of Independence was created in 1745.", a: "peanuts" },
+    { q: "Bananas are vegetables.", a: "cheetos" }
+  ];
+//reset the game
+function reset() {
+
+}
+//start game
+function start() {
+
+}
+//timer
+function timer(){
+
+}
+//result screen
+function result() {
+
+}
+//the time it takes the answer slide to move on
+function slideInterval() {
+
+}
