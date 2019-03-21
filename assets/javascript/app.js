f706ac880525158fcac78e025b907d6a7b345025
tstike44 <you@example.com>
2019-03-20 22:17:32 -0700

    made the check boxes work for each line (still shows option 1 as the default)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,16 +10,12 @@
 //result screen shows the time that was remaining when the answer was clicked, whether or not they got the question right or not, the correct asnwer, and a gif
 //questions don't need to randomize 
 var questions = [
-    { q: "The sky is blue.", a: "bologna" },
-    { q: "There are 365 days in a year.", a: "cheetos" },
-    { q: "There are 42 ounces in a pound.", a: "macaroni" },
-    { q: "The Declaration of Independence was created in 1745.", a: "peanuts" },
-    { q: "Bananas are vegetables.", a: "cheetos" }
+    { q: "", a: "" },
+    { q: ".", a: "" },
+    { q: ".", a: "" },
+    { q: ".", a: "" },
+    { q: "", a: "" }
   ];
-//reset the game
-function reset() {
-
-}
 //start game
 function start() {
 
